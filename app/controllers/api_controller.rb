643d4bee058a300a2329cb5e5a894a9f52fe1cf3
Micah Snyder <micah.snyder@gmail.com>
2011-04-04 14:06:29 -0700

    json structures from Shawn.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,43 @@
 class ApiController < ApplicationController
   require 'net/http'
-  require 'xml'
-  
-  def cabs    
+
+  def cabs
     s = Net::HTTP.get_response(URI.parse('http://cabspotting.org/cabs.xml.php?m=60')).body
     @output = Hash.from_xml(s).to_json
-    
+
     render :layout => false
   end
-  
+
+=begin
+    {
+      :type => 'FeatureCollection',
+      :features => [
+        {
+          :type => 'Feature',
+          :properties => {
+            :cab => cab_id,
+            :status => E|M|?,
+            :time => [timestamps]
+          },
+          :geometry => {
+            :type => 'LineString',
+            :coordinates => [
+              [x, y]
+            ]
+          }
+        }
+      ]
+    }
+=end
+
   def cab
     s = Net::HTTP.get_response(URI.parse("http://cabspotting.org/cab.xml.php?cab=#{params[:cab]}&m=45")).body
+    raw_data = Hash.from_xml(s).map do |item|
+
+    end
+
     @output = Hash.from_xml(s).to_json
-    
+
     render :layout => false
   end
 end
